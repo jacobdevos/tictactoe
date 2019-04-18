02007965394033242d719577f2a718a623d5482b
Jacob de Vos <jacobwdv@ca.ibm.com>
2019-04-18 12:02:34 -0400

    separating game logic from game board

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -1,14 +1,15 @@
 import copy
 
+
 class TicTacToeBoard:
     def __init__(self):
-        self.board=[[None, None, None],[None, None, None],[None, None, None]]
+        self.board = [[None, None, None], [None, None, None], [None, None, None]]
 
     def isGameOver(self):
-        return  self.isGameWon() or self.isCatsGame()
+        return self.isGameWon() or self.isCatsGame()
 
     def isCatsGame(self):
-        if(not self.isGameWon()):
+        if (not self.isGameWon()):
             for row in self.board:
                 for item in row:
                     if not item:
@@ -18,7 +19,7 @@ class TicTacToeBoard:
 
     def isGameWon(self):
         isWon = self.isCrossWon()
-        if(not isWon):
+        if (not isWon):
             for i in range(3):
                 isWon = self.isRowWon(i) or self.isColumnWon(i)
                 if isWon:
@@ -29,34 +30,35 @@ class TicTacToeBoard:
             return False
 
     def isRowWon(self, row):
-        return None != self.board[row][0] and self.board[row][0] == self.board[row][1] == self.board[row][2]
+        return self.board[row][0] is not None and self.board[row][0] == self.board[row][1] == self.board[row][2]
 
     def isColumnWon(self, column):
-        return None != self.board[0][column] and self.board[0][column] == self.board[1][column] == self.board[2][column]
+        return self.board[0][column] is not None and self.board[0][column] == self.board[1][column] == self.board[2][
+            column]
 
     def isCrossWon(self):
-        return None != self.board[0][0] and self.board[0][0] == self.board[1][1] == self.board[2][2] or None != \
+        return self.board[0][0] is not None and self.board[0][0] == self.board[1][1] == self.board[2][2] or None != \
                self.board[0][2] and self.board[0][2] == self.board[1][1] == self.board[2][0]
 
     def putX(self, x, y):
-        if not self.isPointTaken(x,y):
+        if not self.isPointTaken(x, y):
             self.board[x][y] = "X"
         else:
-            print("cannot put X in {},{}".format(x,y))
+            print("cannot put X in {},{}".format(x, y))
 
     def putO(self, x, y):
         if not self.isPointTaken(x, y):
             self.board[x][y] = "O"
         else:
-            print("cannot put O in {},{}".format(x,y))
+            print("cannot put O in {},{}".format(x, y))
 
-    def isPointTaken(self,x,y):
-        return self.board[x][y] != None
+    def isPointTaken(self, x, y):
+        return self.board[x][y] is not None
 
     def printBoard(self, pointer_location):
         display_board = copy.deepcopy(self.board)
         current_value_at_pointer = display_board[pointer_location[0]][pointer_location[1]]
-        if current_value_at_pointer != None:
+        if current_value_at_pointer is not None:
             display_board[pointer_location[0]][pointer_location[1]] = "*" + str(current_value_at_pointer) + "*"
         else:
             display_board[pointer_location[0]][pointer_location[1]] = "**"
@@ -73,7 +75,3 @@ class TicTacToeBoard:
             return " {} ".format(value)
         else:
             return "   "
-
-
-
-
